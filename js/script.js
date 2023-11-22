66d8f5ceddd9d2dd0127b20c79bc7aeee9e060ad
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-22 15:09:59 +0100

    Optimized the code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // punto 3
 let prezzo;
 
@@ -9,23 +8,20 @@ button.addEventListener('click', function(){
     let km = document.getElementById('km').value;
     let age = document.getElementById('age').value;
     let utente = document.getElementById('utente').value;
-
+    
+    // punto 4 che contiene punto 5, 6, 8 e 9
     if(age == 'min'){
         prezzo = ((0.21 * km) * 0.8).toFixed(2);
-        console.log(prezzo);
-        document.getElementById('prezzo_finale').innerText = prezzo + '€';
-        document.getElementById('name').innerText = utente;
     }
     else if(age == 'over'){
         prezzo = ((0.21 * km) * 0.6).toFixed(2);
-        console.log(prezzo);
-        document.getElementById('prezzo_finale').innerText = prezzo + '€'
-        document.getElementById('name').innerText = utente;
     }
     else{
-        prezzo = (0.21 * km).toFixed(2);
-        console.log(prezzo);
-        document.getElementById('prezzo_finale').innerText = prezzo + '€'
-        document.getElementById('name').innerText = utente;
+        prezzo = (0.21 * km).toFixed(2); 
     }
+
+    console.log(prezzo);
+    
+    document.getElementById('prezzo_finale').innerText = prezzo + '€';
+    document.getElementById('name').innerText = utente;
 })
